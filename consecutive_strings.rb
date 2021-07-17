2d3ce67d311cce5689a14f381c392c72c9d88347
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-07-17 14:24:35 +0800

    consecutive strings

---
+++ consecutive_strings.rb
@@ -0,0 +1,24 @@
+def longest_consec(strarr, k)
+  return "" unless k.between?(1, strarr.size)
+  return strarr.each_cons(k).map(&:join).max_by(&:size)
+end
+
+def longest_consec(strarr, k)
+  return "" unless k.between?(1, strarr.length)
+  strarr.each_cons(k).map(&:join).max_by(&:length) || ""
+end
+
+def longest_consec(strarr, k)
+  k > strarr.size || k <= 0 ? '' : strarr.each_cons(k).map(&:join).max_by(&:size)
+end
+
+def longest_consec(strarr, k)
+  return "" if strarr.length == 0 || k > strarr.length || k <= 0
+
+  longest_string = ""
+  strarr.each_index do |i|
+    str = strarr[i...i+k].join
+    longest_string = str if str.length > longest_string.length
+  end
+  longest_string
+end
