c99de48d95546db67ae1a7ac779d0b442dcb3014
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-07-18 09:42:29 +0800

    max sequence

---
+++ max_sequence.rb
@@ -0,0 +1,33 @@
+def max_sequence(a)
+  #your code here
+  max_ending_sum = max_slice_sum =0
+  n=a.length
+  for i in 0...n
+    max_ending_sum = [0,(max_ending_sum+a[i])].max
+    max_slice_sum = [max_slice_sum,max_ending_sum].max
+  end
+  return  max_slice_sum
+end
+
+# other solution
+def max_sequence(array)
+  (1..array.size).map { |i| array.each_cons(i).map(&:sum) }.flatten.push(0).max
+end
+
+# another solution
+def max_sequence(arr)
+  sum = 0
+  max = 0
+  for i in 0...arr.length
+    sum += arr[i]
+
+    if sum < 0
+      sum = 0
+    end
+
+    if sum > max
+      max = sum
+    end
+  end
+  max
+end
