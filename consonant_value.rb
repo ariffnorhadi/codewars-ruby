d555839f434e5c6106d5409ce9a37542d684bf4f
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-07-21 10:36:28 +0800

    consonant value

---
+++ consonant_value.rb
@@ -0,0 +1,10 @@
+def solve(s)
+  s.split(/[aeiou]/).map{|x| x.sum - x.size * 96}.max
+end
+
+def solve(str)
+  str
+    .scan(/[^aeiou]+/)
+    .map { |c| c.chars.map(&:ord).sum - (96 * c.size) }
+    .max
+end
