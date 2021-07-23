f9d6c21df5a3a0840d133dfe21996c1b80754b3a
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-07-23 14:15:45 +0800

    expanded form

---
+++ expanded_form.rb
@@ -0,0 +1,19 @@
+def expanded_form(num)
+  num.digits.each_with_index.map{| val, idx | val * (10 ** idx)}.select{|x| x != 0}.reverse.join(" + ")
+end
+
+# better solution
+
+def expanded_form(num)
+  num.to_s
+     .chars
+     .reverse
+     .map.with_index { |d, idx| d.to_i * 10**idx }
+     .reject(&:zero?)
+     .reverse
+     .join (' + ')
+end
+
+def expanded_form(num)
+  num.to_s.chars.reverse.map.with_index{|i,j| i == "0" ? nil : i + "0" * j }.compact.reverse.join(" + ")
+end
