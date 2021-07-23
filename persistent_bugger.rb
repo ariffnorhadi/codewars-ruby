eb0ef742458a4db9e9c7ea7d7b81184f9b54373d
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-07-23 10:45:29 +0800

    persistent bugger

---
+++ persistent_bugger.rb
@@ -0,0 +1,18 @@
+def persistence(n)
+  k = 0
+  while n > 9 do
+    n = n.to_s.split(//).map { |x| x.to_i }.inject(:*)
+    k+=1
+  end
+  k
+end
+
+# better solution
+def persistence(n)
+  n < 10 ? 0 : 1 + persistence(n.to_s.chars.map(&:to_i).reduce(:*))
+end
+
+def persistence(n)
+  return 0 if n < 10
+  1 + persistence(n.digits.reduce(&:*))
+end
