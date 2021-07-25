64f1edc8c9989ef26917c3ea21de4ea174ce842f
Muhammad Ariff Bin Norhadi <ariffnorhadi@gmail.com>
2021-07-25 08:38:28 +0800

    gap in prime

---
+++ gap_in_prime.rb
@@ -0,0 +1,24 @@
+def gap(g, m, n)
+  (m..n).each do |i|
+    next unless is_prime(i)
+    return [i, i+g] if is_prime(i+g) && (i+1..i+g-1).none? { |num| is_prime num }
+  end
+  nil
+end
+
+def is_prime(n)
+  (2..Math.sqrt(n).round).each { |i| return false if n % i == 0 }
+  true
+end
+
+# another solution
+require 'prime'
+
+def gap(gap, low, high)
+  primes_between(low,high).each_cons(2).find { |(a,b)| b-a == gap }
+end
+
+def primes_between(low,high)
+  Prime.each(high).select { |prime| prime >= low }
+end
+
